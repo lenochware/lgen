e954bdc341f697416718db2bef14e3b8bf1e908a
lenochware <lenochware@gmail.com>
2022-07-12 15:09:35 +0200

    Tunnel - doors

--- libs/Tunnel.php
+++ libs/Tunnel.php
@@ -29,44 +29,40 @@ class Tunnel extends Entity
     if ($a == $b) return $b;
 
     $len = $b[0] - $a[0];
-    for($i = 0; $i < $len*sign($len); $i++) {
-      $this->dig($a[0] + $i*sign($len), $a[1]);
+    $prevX = $a[0];
+    
+    for($i = 0; $i < abs($len); $i++) {
+      $x = $a[0] + $i*sign($len);
+      $this->dig($x, $a[1], $prevX, $a[1]);
+      $prevX = $x;
     }
 
     $len = $b[1] - $a[1];
-    for($i = 0; $i < $len*sign($len); $i++) {
-      $this->dig($b[0], $a[1] + $i*sign($len));
+    $prevY = $a[1];
+    
+    for($i = 0; $i < abs($len); $i++) {
+      $y = $a[1] + $i*sign($len);
+      $this->dig($b[0], $y, $b[0], $prevY);
+      $prevY = $y;
     }
     
     return $b;
   }
 
-  protected function dig($x, $y)
+  protected function dig($x, $y, $prevX, $prevY)
   {
-    static $door, $outside = false;
-
     $tile = $this->level->get($x, $y);
-
-    if ($tile[3] == 'room-wall')
-    {
-      if ($door and $outside) return;
-      if ($door and !$outside) {
-        $this->level->set($door[0], $door[1], 'wall');
-      };
-
-      $id =  'door';
-      $door = [$x, $y];
-    }
-    else $id = 'floor';
-
+    $prev = $this->level->get($prevX, $prevY);
 
     if ($tile[3] == 'outside') {
       $this->level->set($x, $y, 'tunnel');
-      $outside = true;
-      $door = null;
     }
 
-    $this->level->set($x, $y, $id);
+    $this->level->set($x, $y, 'floor');
+
+    if (($prev[3] == 'room-floor' or $prev[3] == 'tunnel') and $tile[3] == 'room-wall') {
+      $this->level->set($x, $y, 'door');
+    }
   }
 
 }
