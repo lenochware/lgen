fde02a14ae4597f956642167d55e778aa95e5b42
lenochware <lenochware@gmail.com>
2022-07-18 14:32:10 +0200

    wild: forest

--- libs/WildRoom.php
+++ libs/WildRoom.php
@@ -5,7 +5,6 @@ class WildRoom extends Room
 
   function create()
   {
-
     $this->type = $this->random->get(['desert', 'forest', 'swamp', 'rocks']);
     $this->type = 'forest';
 
@@ -17,28 +16,33 @@ class WildRoom extends Room
   	$this->fill('room-floor', 'grass');
   }
 
+  protected function replaceOutsideFunc($id)
+  {
+    $func = function($level, $x, $y) use($id) {
+      $tile = $level->get($x, $y);
+      if (!$tile) return 'none';
+      if ($tile[3] == 'outside' or $tile[3] == 'tunnel') return $id;
+      return 'none';
+    };
+
+    return $func;
+  }
+
   function createForest()
   {
   	//$this->clear(['floor', '', '', 'outside']);
   	$this->fill('granite-wall', 'floor');
   	$this->spread('outside', 'tree', 20);
-  	$this->fill('room-floor', 'grass');
+  	$this->spread('room-floor', rfunc('', ['mud','grass']), rint(0,10));
+    
+  	//pool
+    if ($this->random->pass($this->lvl, [1=>.1])) {
+      $this->pool(rint(0,12), rint(0,12), $this->replaceOutsideFunc('water'), rint(5,15));
+    }
 
-  	$func = function($level, $x, $y) {
-  		$tile = $level->get($x, $y);
-  		if (!$tile) return 'none';
-  		if ($tile[3] == 'outside' or $tile[3] == 'tunnel') return 'water';
-  		return 'none';
-  	};
-
-    if (rbet(.5))
-    	$this->pool(rint(0,12), rint(0,12), rfunc('', ['tree','grass']), rint(5,15));
-    else 
-    	$this->pool(rint(0,12), rint(0,12), $func, rint(5,15));
-
+    $this->spread('floor', rfunc('i2', 'animal'), rint(0,4));
     
     // $this->spread('water', 'fish', 20);
-    // $this->pool(0, 1, rfunc('', ['tree','grass']));
   }
 
   function createSwamp()
