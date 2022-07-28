64355092285c084ecb28360cc950970bdfca1fcd
lenochware <lenochware@gmail.com>
2022-07-28 16:53:11 +0200

    doc

--- libs/Random.php
+++ libs/Random.php
@@ -10,6 +10,7 @@ function __construct()
   $this->seed = time();
 }
 
+/** Return random float in interval <0,1> */
 function float()
 {
   $x = sin($this->seed++) * 10000;
@@ -21,21 +22,25 @@ function integer($max)
   return floor($this->float() * ++$max);
 }
 
+/** Return true with probability $p, for 50% use chance(0.5) */
 function chance($p)
 {
   return ($this->float() < $p);
 }
 
+/** Get random item from the list. */
 function get($list)
 {
   return $list[$this->int(0,count($list)-1)];
 }
 
+/** Get random item from the list (lower index with higher probability). */
 function get2($list)
 {
   return $list[$this->int2(0,count($list)-1)];
 }
 
+/** Get random integer in interval. */
 function int($a, $b = null)
 {
   if (is_array($a)) return $this->int($a[0], $a[1]);
@@ -48,11 +53,13 @@ function int2($a, $b = null)
   return $this->int($a, $this->int($a, $b));
 }
 
+/** Shuffle array in place. */
 function shuffle(&$list)
 {
   usort($list, fn() => sign(0.5 - $this->float()));
 }
 
+/** Pick item from list with probability using chances array. Or you can use list [item,chance,item,chance]. */
 function pick($list, $chances = null)
 {
   if ($chances) return $this->getc($list, $chances);
@@ -68,6 +75,7 @@ function pick($list, $chances = null)
   return $this->getc($items, $chances);
 }
 
+/** Helper for pick() */
 function getc($items, $chances)
 { 
   $max = $this->float() * array_sum($chances);
@@ -79,6 +87,7 @@ function getc($items, $chances)
   }
 }
 
+/** Pick $index item from $list. If $index does not exists, it approximates between list items. */
 protected function pickList($index, $list)
 {
   $index = (int)$index;
@@ -95,11 +104,13 @@ protected function pickList($index, $list)
   }
 }
 
+/** chance() with probabilities driven by $list. pass($lvl, [1=>0.1,3=>0.5,6=>0]) */
 function pass($index, $list)
 {
   return $this->chance($this->pickList($index, $list));
 }
 
+/** Return callback for random selection. put(x, $y, func('',['wall','wall-moss'])) */
 function func($type, $list /*, $chances = null*/)
 {
   if ($type == 'i2') {
