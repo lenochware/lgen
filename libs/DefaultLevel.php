9145bfaae9225b90c29a42111c1af87071ae046b
lenochware <lenochware@gmail.com>
2022-08-19 12:08:36 +0200

    x ***Room classes

--- libs/DefaultLevel.php
+++ libs/DefaultLevel.php
@@ -40,6 +40,27 @@ class DefaultLevel extends Level
     $room->spread('room-floor', rfunc('', ['wet-floor','floor']), rint(1,5));
     $room->spread('water', rfunc('', 'water-list'), rint(0,2));
     $room->spread('tunnel', rfunc('', ['wet-floor']), rint(1,5));
+  }
+
+  function populateDestruct($room)
+  {
+    $room->spread('room-floor', rfunc('', ['wall','small-rock']), rint(1,5));
+    $room->spread('room-wall', rfunc('', ['dirt','floor']), rint(1,5));
+    $room->spread('tunnel', rfunc('', ['small-rock']), rint(1,5));
+  }
+
+  function populatePit($room)
+  {
+    $obj = dbget(rget('actor'));
+
+    $room->fill('room-floor', rfunc('', $obj['family'][1]));
+    $room->spread('tunnel', rfunc('', ['blood-floor', 'bones']), rint(1,5));
+    //$room->fill('room-floor', rget('actor'));
+  }
+
+  function populateTreasure($room)
+  {
+    $room->spread('room-floor', rfunc('', ['copper-coins','silver-coins']), rint(1,5));
   }  
 
 }
