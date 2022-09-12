7492a3624af10edbbca06d474024c7626630bf61
lenochware <lenochware@gmail.com>
2022-09-12 15:29:58 +0200

    cellars-2

--- libs/CellarsLevel.php
+++ libs/CellarsLevel.php
@@ -6,26 +6,36 @@ class CellarsLevel extends Level
   {
     parent::__construct('cellars', $number);
   }
-  
-  function create()
+
+  function initLevel()
   {
     $this->init(5,5);
 
-    switch ($this->number)  {
-      case 1: $types = ['empty', 5, 'wet', 5, 'destruct', 2, 'warehouse', 1]; break;
-      case 2: $types = [ 'destruct', 5, 'warehouse', 1]; break;
-      case 3: $types = [ 'destruct', 5, 'big', 5, 'warehouse', 1]; break;
-      default:
-        $types = ['empty', 5, 'wet', 5, 'destruct', 2, 'warehouse', 1];
+    if ($this->number == 1)  {
+      $types = ['empty', 5, 'wet', 1, 'destruct', 1, 'warehouse', 1];
+    } else {
+      $types = ['empty', 5, 'wet', 2, 'destruct', 5, 'warehouse', 1];
     }
 
-    $this->connect();
+    $this->config['room-types'] = $types;
 
-    //if ($this->number > 1 /* and rbet(.5)*/) $this->addTag('big-rooms');
+    if ($this->number > 1 and rbet(.1) ) $this->addTag('wet-level');
+
+    if ($this->number == 3) {
+      $room = rget($this->sectors)->room;
+      $room->addTag('boss');
+    };
+
+  }
+  
+  function create()
+  {
+    $this->initLevel();
+    $this->connect();
 
     foreach ($this->sectors as $sector)
     {
-      $type = $this->random->pick($types);
+      $type = $this->random->pick($this->config['room-types']);
 
       $room = $sector->room;
       $room->init($type);
@@ -36,10 +46,12 @@ class CellarsLevel extends Level
 
     $this->addExits();
     
-    foreach ($this->sectors as $sector) {
-
-      $sector->room->spread('room-floor', 'water', rint(1,5));
-      $sector->room->spread('tunnel', 'water', rint(1,5));
+    foreach ($this->sectors as $sector)
+    {
+      if ($this->is('wet-level')) {
+        $sector->room->spread('room-floor', 'water', rint(1,5));
+        $sector->room->spread('tunnel', 'water', rint(1,5));  
+      }
 
       if ($sector->is('exit')) {
         $this->populate($sector->room, 'exit');
@@ -95,15 +107,23 @@ class CellarsLevel extends Level
     $room->spread('room-wall', rfunc('', ['dirt','floor']), rint(1,5));
     $room->spread('tunnel', rfunc('', ['small-rock']), rint(0,2));
     $room->spread('room-floor', rfunc('', ['rat', 'old-bread']), rint(0,3));
+
+    if ($this->number > 1) {
+      $room->spread('tunnel', 'rat', rint(0,3));
+    }
   }
 
   function populateEmpty($room)
   {
     $room->spread('room-floor', rfunc('', ['bones', 'dirty-rag']), rint(0,1));
 
+    $items = ['cheese', 'old-bread'];
+    if ($this->number > 1) $items += ['slime', 'beer'];
+    if ($this->number > 2) $items += ['rat-meat'];
+
     if (rbet(.1)) {
-      $room->spread('room-floor', 'rat', 1);
-      $room->spread('room-floor', rget(['cheese', 'old-bread']) , 1);
+      $room->spread('room-floor', 'rat', rint(1, $this->number));
+      $room->spread('room-floor', rfunc('', $items) , rint(1, $this->number));
     }
   }
 
